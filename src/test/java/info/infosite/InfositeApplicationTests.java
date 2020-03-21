97e7d2bc2c84f80414153813328b30cfc2336b36
Alex <alexeydemeshko@gmail.com>
2020-03-21 12:15:18 +0300

    Initial commit

---
+++ src/test/java/info/infosite/InfositeApplicationTests.java
@@ -0,0 +1,13 @@
+package info.infosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
