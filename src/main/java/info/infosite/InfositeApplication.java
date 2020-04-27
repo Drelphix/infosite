9f5b5c3e6bcbb055eb44cbbbbde6c88ea2fc4bcc
Alex <alexeydemeshko@gmail.com>
2020-04-27 19:03:53 +0300

    Работает

--- src/main/java/info/infosite/InfositeApplication.java
+++ src/main/java/info/infosite/InfositeApplication.java
@@ -2,10 +2,8 @@ package info.infosite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"info.infosite"})
 public class InfositeApplication {
 
 	public static void main(String[] args) {
