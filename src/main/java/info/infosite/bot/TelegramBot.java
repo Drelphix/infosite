7a5cd254baafcae83b1a49fdc6807b2ec4a22410
Alex <alexeydemeshko@gmail.com>
2020-10-07 18:57:31 +0300

    Фиксы ссылок
    Signed-off-by: Alex <alexeydemeshko@gmail.com>

--- src/main/java/info/infosite/bot/TelegramBot.java
+++ src/main/java/info/infosite/bot/TelegramBot.java
@@ -8,16 +8,21 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 @EnableAsync
 public class TelegramBot extends TelegramLongPollingBot {
     @Autowired
     UserRepository userRepository;
+    public final Queue<Object> sendQueue = new ConcurrentLinkedQueue<>();
+    public final Queue<Object> receiveQueue = new ConcurrentLinkedQueue<>();
 
     @SneakyThrows
     @Override
     @Async
     public void onUpdateReceived(Update update) {
-
+        receiveQueue.add(update);
     }
 
     @Override
