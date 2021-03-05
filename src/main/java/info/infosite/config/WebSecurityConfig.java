ed88d65c760c425fef7f5b75f42ed6164dbca112
Alex <alexeydemeshko@gmail.com>
2021-03-05 16:17:45 +0300

    HTTPS
    Signed-off-by: Alex <alexeydemeshko@gmail.com>

--- src/main/java/info/infosite/config/WebSecurityConfig.java
+++ src/main/java/info/infosite/config/WebSecurityConfig.java
@@ -21,6 +21,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure();
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/registration").permitAll()
